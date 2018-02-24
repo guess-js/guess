230be953a880ca5e3b3ff56f30a88c020f8c8b13
mgechev <mgechev@gmail.com>
2018-02-24 15:54:54 -0800

    Improve visualization

--- lib/report/index.ts
+++ lib/report/index.ts
@@ -4,6 +4,7 @@ import * as express from 'express';
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.static('node_modules'));
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/reports/:id', async (req, res) => res.json(await dbStorage(req.params.id).all()));
 
