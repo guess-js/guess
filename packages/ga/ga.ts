da3f0fafbf4c350f9a63993036d5d006d7736f01
mgechev <mgechev@gmail.com>
2018-04-02 19:57:40 -0700

    refactor: update apis

--- packages/ga/ga.ts
+++ packages/ga/ga.ts
@@ -6,32 +6,8 @@ const PageSize = 1000;
 const noop = (r: string) => r;
 const DefaultExpression = 'ga:users';
 
-async function fetchData(
-  routeDeclarations: string[],
-  formatter: (r: string) => string,
-  jwtClient: any,
-  viewId: string,
-  period: Period,
-  expression: string
-) {
-  const client = getClient(jwtClient, PageSize, viewId, period, expression);
-  const graph: Graph = {};
-  for await (const val of client()) {
-    if (val.error) {
-      throw val.error;
-    }
-    const result = val.report;
-    normalize(result.data, formatter, routeDeclarations).forEach((n: any) => {
-      const r = graph[n.from] || {};
-      r[n.to] = n.weight + (r[n.to] || 0);
-      graph[n.from] = r;
-    });
-  }
-  return graph;
-}
-
 export interface FetchConfig {
-  key: any;
+  auth: any;
   viewId: string;
   period: Period;
   formatter?: (route: string) => string;
@@ -39,31 +15,19 @@ export interface FetchConfig {
   expression?: string;
 }
 
-export function fetch(config: FetchConfig): Promise<Graph> {
-  return new Promise((resolve, reject) => {
-    const { google } = require('googleapis');
-
-    const jwtClient = new google.auth.JWT(
-      config.key.client_email,
-      null,
-      config.key.private_key,
-      ['https://www.googleapis.com/auth/analytics.readonly'],
-      null
-    );
-
-    jwtClient.authorize(function(err: any, tokens: any) {
-      if (err) {
-        reject(err);
-        return;
-      }
-      fetchData(
-        config.routes || [],
-        config.formatter || noop,
-        jwtClient,
-        config.viewId,
-        config.period,
-        config.expression || DefaultExpression
-      ).then(resolve, reject);
+export async function fetch(config: FetchConfig) {
+  const client = getClient(config.auth, PageSize, config.viewId, config.period, config.expression || DefaultExpression);
+  const graph: Graph = {};
+  for await (const val of client()) {
+    if (val.error) {
+      throw val.error;
+    }
+    const result = val.report;
+    normalize(result.data, config.formatter || noop, config.routes || []).forEach((n: any) => {
+      const r = graph[n.from] || {};
+      r[n.to] = n.weight + (r[n.to] || 0);
+      graph[n.from] = r;
     });
-  });
+  }
+  return graph;
 }
