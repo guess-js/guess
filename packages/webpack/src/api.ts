3b5ad8d39e591e31b77afee752579211de1cf28f
mgechev <mgechev@gmail.com>
2018-05-07 08:01:20 -0700

    Guess now does not depend on window

--- packages/webpack/src/api.ts
+++ packages/webpack/src/api.ts
@@ -1,1 +1,2 @@
-export const guess = (current: string, links?: string[]) => (window as any).__GUESS__.guess(current, links);
+export const guess = (current: string, links?: string[]) =>
+  ((typeof window === 'undefined' ? global : window) as any).__GUESS__.guess(current, links);
