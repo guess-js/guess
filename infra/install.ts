5f1dfc97f1c92ecefe8228b5c95e67360e1bdd28
mgechev <mgechev@gmail.com>
2019-05-12 07:31:27 -0700

    build: fix dependencies

--- infra/install.ts
+++ infra/install.ts
@@ -2,4 +2,8 @@ import { join } from 'path';
 import { execSync } from 'child_process';
 
 const PackagesDir = join(process.cwd(), 'packages');
-console.log(execSync(`cd ${join(PackagesDir, 'guess-parser', 'test', 'fixtures', 'angular')} && npm i`).toString());
+console.log(
+  execSync(
+    `cd ${join(PackagesDir, 'guess-parser', 'test', 'fixtures', 'angular')} && npm i`
+  ).toString()
+);
