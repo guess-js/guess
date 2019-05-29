d6562ffcb436f53bfba9ccc70921a61e7e0d8bba
mgechev <mgechev@gmail.com>
2019-05-29 19:55:25 +0200

    Add Angular e2e test

---
+++ infra/e2e.ts
@@ -0,0 +1,13 @@
+import { execSync } from 'child_process';
+import { readFileSync } from 'fs';
+
+const enterTest = 'cd packages/guess-webpack/test/fixtures/angular';
+execSync(`${enterTest} && npm i`);
+execSync(
+  `${enterTest} && ./node_modules/.bin/ng build --extra-webpack-config webpack.extra.js`
+);
+
+const fooModule = readFileSync('packages/guess-webpack/test/fixtures/angular/dist/angular/foo-foo-module.js').toString();
+if (fooModule.indexOf('__GUESS__.p(["baz-baz-module.js",1]') < 0) {
+  process.exit(1);
+}
